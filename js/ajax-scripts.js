4da651f8c82cc80bed6dd355fadd8d60eacc6793
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-22 14:48:36 -0800

    My Javascript File

---
+++ js/ajax-scripts.js
@@ -0,0 +1,30 @@
+/** 
+ *	@author Arvin Kent Lazaga <arvinkent17@gmail.com> 
+ *  This Javascript File Contains All AJAX Scripts for Manipulating Data 
+ */
+
+// Initialize All Functions on DOM 
+$(function() {
+	init();
+})
+
+// Automatically Call Timeout for Data Update
+function init() {
+	setTimeout(function() {
+	auto_update();
+	init();	
+	}, 200);
+}
+
+// Retrieve Data from Database using JSON
+function auto_update() {
+	$.getJSON('fetchdata.php', function(data) {
+		$('ul').empty();
+
+		$.each(data.dataresult, function() {
+			$('ul').append('<li>Lastname: '+this['lname']+'</li><li>Firstname: '+this['fname']+'</li><li>Middlename: '+this['mname']+'</li>');
+			
+		});
+		$('h1').text('');
+	});
+}
