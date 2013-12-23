fe1ca319648a731e2d97bfc7e11ef87e1d53ab5a
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-23 03:53:52 -0800

    File Directory Controller | Default

---
+++ includes/functions/initialize.php
@@ -0,0 +1,28 @@
+<?php
+
+	/**
+     * This File Contains All File Controlling Directories.
+     * 
+     * @author Arvin Kent Lazaga <arvinkent17@gmail.com>
+     * @copyright Arvin Kent Lazaga December 23, 2013
+     * @since December 23, 2013
+	 */
+
+	/**
+	 * File Directory Controller.
+	 * 
+	 * constant DS, constant SITE_ROOT, constant PATH1, constant PATH2, constant PATH3
+	 * 
+	 */
+	defined("DS") ? NULL : define("DS", DIRECTORY_SEPARATOR);
+	defined("SITE_ROOT") ? NULL : define("SITE_ROOT", DS."xampp".DS."htdocs".DS."E-Shop");
+	defined("PATH1") ? NULL : define("PATH1", SITE_ROOT.DS."includes".DS."classes");
+	defined("PATH2") ? NULL : define("PATH1", SITE_ROOT.DS."includes".DS."functions");
+	defined("PATH3") ? NULL : define("PATH1", SITE_ROOT.DS."includes".DS."layouts");
+
+	/**
+	 * @global require_once load functions.php file  
+	 */
+	require_once(PATH2.DS."functions.php"); 
+
+?>
