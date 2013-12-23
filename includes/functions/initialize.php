77643b962f06de0554888c20f84e8f2a00856be4
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-23 03:56:38 -0800

    File Directory Controller | Default

--- includes/functions/initialize.php
+++ includes/functions/initialize.php
@@ -17,8 +17,8 @@
 	defined("DS") ? NULL : define("DS", DIRECTORY_SEPARATOR);
 	defined("SITE_ROOT") ? NULL : define("SITE_ROOT", DS."xampp".DS."htdocs".DS."E-Shop");
 	defined("PATH1") ? NULL : define("PATH1", SITE_ROOT.DS."includes".DS."classes");
-	defined("PATH2") ? NULL : define("PATH1", SITE_ROOT.DS."includes".DS."functions");
-	defined("PATH3") ? NULL : define("PATH1", SITE_ROOT.DS."includes".DS."layouts");
+	defined("PATH2") ? NULL : define("PATH2", SITE_ROOT.DS."includes".DS."functions");
+	defined("PATH3") ? NULL : define("PATH3", SITE_ROOT.DS."includes".DS."layouts");
 
 	/**
 	 * @global require_once load functions.php file  
