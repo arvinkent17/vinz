b453cbceddbf7087fbc484e1ca30f7565654e50c
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-23 03:52:16 -0800

    Database Contants | Default

---
+++ includes/functions/config.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+     * This File Contains Database Constants.
+     * 
+     * @author Arvin Kent Lazaga <arvinkent17@gmail.com>
+     * @copyright Arvin Kent Lazaga December 23, 2013
+     * @since December 23, 2013
+	 */
+
+	/**
+	 * Database Constants.
+	 * 
+	 * constant DB_SERVER, constant DB_USER, constant DB_PASS, constant DB_NAME
+	 * 
+	 */
+	defined("DB_SERVER") ? NULL : define("DB_SERVER", "localhost");
+	defined("DB_USER") ? NULL : define("DB_USER", "root");
+	defined("DB_PASS") ? NULL : define("DB_PASS", "");
+	defined("DB_NAME") ? NULL : define("DB_NAME", "eshop");
+	
+?>
