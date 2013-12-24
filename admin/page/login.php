7a37d14954f44a343c6da174ae676984d98933f3
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-24 03:07:18 -0800

    Admin | Login Page - not yet finished

--- admin/page/login.php
+++ admin/page/login.php
@@ -1,9 +1,18 @@
+<?php 
+	
+	/**
+	 * @global require_once Calls File Directory Controller
+	 */
+	require_once("../../includes/functions/initialize.php");
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
 		<title>Welcome to E-Shopping | The #1 Online Shopping Website daw</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="author" content="Arvin Kent Lazaga">
 		<!-- Twitter Bootstrap External CSS Files -->
 		<link href="../../css/bootstrap.min.css" rel="stylesheet">
 		<link href="../../css/bootstrap-theme.min.css" rel="stylesheet">
@@ -15,15 +24,18 @@
 		<!-- Navigation Starts Here -->
 		<div class="navbar navbar-inverse navbar-static-top">
 			<div class="container">
-				<a href="index.php" class="navbar-brand">E-Shopping</a>
+				<a href="../index.php" class="navbar-brand">E-Shop</a>
 			</div>
 		</div>
 		<!-- Navigation Ends Here -->
 
+
 		<!-- Footer Starts Here -->
 		<div class="navbar navbar-default navbar-fixed-bottom">
 			<div class="container">
-				<p class="navbar-text pull-left">&copy;Copyright 2013 <br /> Powered by Twitter Bootstrap 3.0 <br /> Site Built by Arvin Kent Lazaga</p>
+				<div class="row">
+					<p class="navbar-text pull-left">&copy;Copyright 2013 <br /> Powered by Twitter Bootstrap 3.0 <br />Design and Built by Arvin Kent Lazaga</p>
+				</div>
 			</div>
 		</div>
 		<!-- Footer Ends Here -->
@@ -31,22 +43,30 @@
 		<!-- Admin Panel List -->
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-9">
+				<div class="col-lg-8">
 					<div class="panel panel-default">
 						<div class="panel-body">
 							<div class="page-header">
-								<h3>Current Admins</h3>
+								<h3>Administrator List</h3>
+							</div>	
+							<div id="fix-bottom" class="jumbotron">
+								<h3>Welcome to Administrator Login Page</h3> 
+								<p>Note: Before Logging in to the System make sure that you are now in a private place</p>
+								<a class="btn btn-info" href="../../index.php">Back to Main Site</a>	
+								<div class="pull-right">
+									<h4>By Administrator Arvin Kent</h4>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 				<!-- Login Form Starts Here -->
-				<div class="col-lg-3">
+				<div class="col-lg-4">
 					<div class="list-group">
 						<div class="list-group-item">
-							<h4 class="list-group-item-heading">Administrator Login</h4>
+							<h3 class="list-group-item-heading">Administrator Login</h3>
 							<div id="groupitem" class="list-group-item-text">
-								<form class="form-horizontal" method="post">
+								<form class="form-horizontal" action="authenticate-login.php" method="post">
 									<div class="form-group">
 										<input type="text" class="form-control" placeholder="Username">
 									</div>
@@ -58,6 +78,32 @@
 									</div>
 								</form> 
 							</div>
+						</div>
+						<div class="list-group-item">
+							<h4 class="list-group-item-heading">Total Registered User on Our Site </h4>
+							<br />
+							<p>Currently there is no Registered User Yet</p>
+						</div>
+						<div class="list-group-item">
+							<h4 class="list-group-item-heading">Social Media </h4>
+							<br />
+							<table class="table-condensed" align="center">
+								<tbody>
+									<tr>
+										<td>
+											<a href="#" title="Go to your Facebook Page"><img src="../../img/facebook.png" class="img-responsive"></a>
+										</td>
+										<td>&nbsp;</td>
+										<td>
+											<a href="#" title="Go to your Google+ Page"><img src="../../img/googleplus.png" class="img-responsive"></a>
+										</td>
+										<td>&nbsp;</td>
+										<td>
+											<a href="#" title="Go to your Twitter Page"><img src="../../img/twitter.png" class="img-responsive"></a>
+										</td>
+									</tr>
+								</tbody>
+							</table>						
 						</div>
 					</div>	
 				</div>
@@ -72,3 +118,15 @@
 		<script src="../../js/ajax-scripts.js"></script>
 	</body>
 </html>
+<?php 
+
+	/**
+	 * Closes Connection if Connection was been Established.
+	 *
+	 * Calls Database Object Class Method close_connection 
+	 */
+    if( isset( $db ) ) { 
+    	$db->close_connection(); 
+    } 
+
+?>
