0fc283982c6afe488d78e1a4592446b2584fa688
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-25 03:18:03 -0800

    Fix Configuration

--- includes/functions/config.php
+++ includes/functions/config.php
@@ -17,6 +17,6 @@
 	defined("DB_SERVER") ? NULL : define("DB_SERVER", "localhost");
 	defined("DB_USER") ? NULL : define("DB_USER", "root");
 	defined("DB_PASS") ? NULL : define("DB_PASS", "");
-	defined("DB_NAME") ? NULL : define("DB_NAME", "eshop");
+	defined("DB_NAME") ? NULL : define("DB_NAME", "online_shopping");
 	
 ?>
