789421f8631242e6269dfcd3cb2bc0063e1e7f3d
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-25 03:16:35 -0800

    Administrator Class | Fixed

--- includes/classes/admin.php
+++ includes/classes/admin.php
@@ -15,11 +15,19 @@
 	class Admin {
 
 		/**
+		* Admin Object Variables.
+  		* 
+  		* @access public
+		*/
+		public $id;
+		public $username;
+		public $password;
+
+		/**
 		 * Admin Table Name
 		 *  
 		 * @access protected
 		 */
-		protected $admin = "tbl_admin";
 
 		/**
 		 * Authenticate Admin Username and Password when Logging in
@@ -27,18 +35,59 @@
 		 * @access protected
 		 * @return boolean TRUE or False
 		 */
+		public function find_all() {
+			return $this->find_by_sql( "SELECT * FROM tbl_admin" );
+		}
+
+		public function find_by_id( $id = 0 ) {
+			$result = $this->find_by_sql( "SELECT * FROM tbl_admin WHERE admin_id {$id} LIMIT 1" );
+			return !empty( $result ) ? array_shift( $result ) : false;
+		}
+
+		 /**
+          * Authenticate Admin Username and Password when Logging in.
+          *
+          * @access public
+          * @return boolean TRUE or False
+          */
 		public function authenticate( $username = "", $password = "" ) {
 			global $db;
 			$username = $db->escape_value( $username );
 			$password = $db->escape_value( $password );
-			$query = "SELECT * FROM " . $this->admin;
+			$query = "SELECT * FROM tbl_admin ";
 			$query .= "WHERE username = '{$username}' ";
-			$query .= "AND password = '{$password}' ";
+			$query .= "AND hashed_password = '{$password}' ";
 			$query .= "LIMIT 1";
-			$result = $db->exe_query( $query );
-			return $result;
+			$result = $this->find_by_sql( $query );
+			return !empty( $result ) ? array_shift( $result ) : false;
 		} 
 
+		public function find_by_sql( $sql = "" ) {
+			global $db;
+			$result = $db->exe_query( $sql );
+			$object_array = array();
+			while( $row = $db->fetch_row( $result ) ) {
+				$object_array[] = $this->instantiate( $row );
+			}
+			return $object_array;
+		}
+
+		/**
+		 * Instantiate a new Object of it Self.
+		 *
+		 * Pass the array values of each inside of a object variables
+		 * 
+		 * @access private
+		 * @param array $record
+		 * @return new object  
+		 */
+		private function instantiate( $record ) {
+			$object = new self;
+			$object->id = $record['admin_id'];
+			$object->username = $record['username'];
+			$object->password = $record['hashed_password'];
+			return $object;
+		}
 	}
 
 	/**
