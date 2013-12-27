b5d726ad8f733ae9c2c806142605df40c86660d7
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-26 23:53:19 -0800

    Product Classes | Not yet finished

---
+++ admin/includes/classes/products.php
@@ -0,0 +1,33 @@
+<?php
+	/**
+     * This File Contains User Class.
+     * 
+     * @author Arvin Kent Lazaga <arvinkent17@gmail.com>
+     * @copyright Arvin Kent Lazaga December 26, 2013
+     * @since December 26, 2013
+	 */
+
+	class Products {
+		/**
+		 * Counts Products.
+		 * 
+		 * @access public
+		 * @return total number of Products 
+		 */
+		public function count() {
+			global $db;
+			$result =  $db->exe_query( "SELECT * FROM tbl_products" );
+			$count = $db->num_rows( $result );
+
+			if( $count == 0 ) {
+				
+			}
+			return $count;
+		}
+	}
+
+	/**
+	 * Instantiate the Products Object Class
+	 */
+	$products = new Products();
+?>
