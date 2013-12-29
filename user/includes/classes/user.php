24a05fff6351cdd64a24d82fc3fec7d7efbac409
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-28 20:20:21 -0800

    User Class | Just Added Some More Methods

--- user/includes/classes/user.php
+++ user/includes/classes/user.php
@@ -29,6 +29,90 @@
 			}
 			return $count;
 		}
+
+		/**
+		 * Retrieve Data from Table User
+		 *
+		 * 
+		 * @access public
+		 * @param string $result, string $message
+		 * @throws notification if empty result
+		 */
+		public function retrieve_users( $result , $message = "" ) {
+
+			$output = "";
+
+			global $paginate;
+			global $db;
+
+			if( $db->num_rows( $result ) == 0 ) {
+
+				echo "<td align=center colspan={$rows}><div class=\"alert-message error\">";
+				echo "<h4>{$message}</h4>";
+				echo "</div></td>";
+
+			} else {
+
+				$cnt = $paginate->offset() + 1;			
+
+				while ( $row = $db->fetch_row( $result ) ) {
+					$name = urlencode($row['fname']);
+					echo "<tr valign=center>";
+					echo "<td>{$cnt}</td>";	
+					echo "<td>" . ucfirst($row['fname']) . " " . ucfirst($row['lname']) . " " . ucfirst($row['mname']) . "</td>";
+					echo "<td>" . $row['email'] . "</td>";
+					echo "<td>". $row['addr'] . "</td>";
+					echo "<td>0" . $row['contact_no'] . "</td>";
+					echo "<td>" . $row['username'] . "</td>";
+					echo "<td>
+							  <a class=\"nav-text\" href=\"operations/edit.php?id=" . urlencode($row["user_id"]) . "&name={$name}" . "\">Edit</a>
+						      <a class=\"nav-text\" href=\"operations/delete.php?id=" . urlencode($row["user_id"]) . "&name={$name}" . "\">Delete</a>
+						  </td>";
+						
+					echo "</tr>";
+					$cnt++;
+				}
+			}
+		}
+
+		public function search_user( $result , $message = "" ) {
+
+			$output = "";
+
+			global $paginate;
+			global $db;
+
+			if( $db->num_rows( $result ) == 0 ) {
+
+				echo "<td align=center colspan={$rows}><div class=\"alert-message error\">";
+				echo "<h4>{$message}</h4>";
+				echo "</div></td>";
+
+			} else {
+
+				$cnt = $paginate->offset() + 1;			
+
+				while ( $row = $db->fetch_row( $result ) ) {
+					$productname = urlencode($row['product_name']);
+					echo "<tr valign=center>";
+					echo "<td>{$cnt}</td>";	
+					echo "<td>" . $row['fname'] . "</td>";
+					echo "<td>" . $row['lname'] . "</td>";
+					echo "<td>" . $row['mname'] . "</td>";
+					echo "<td>" . $row['email'] . "</td>";
+					echo "<td>" . $row['addr'] . "</td>";
+					echo "<td>0" . $row['contact_no'] . "</td>";
+					echo "<td>0" . $row['username'] . "</td>";
+					echo "<td>
+							  <a class=\"nav-text\" href=\"operations/edit.php?id=" . urlencode($row["user_id"]) . "&name={$productname}" . "\">Edit</a>
+						      <a class=\"nav-text\" href=\"operations/delete.php?id=" . urlencode($row["user_id"]) . "&name={$productname}" . "\">Delete</a>
+						  </td>";
+						
+					echo "</tr>";
+					$cnt++;
+				}
+			}
+		}
 	}
 
 	/**
