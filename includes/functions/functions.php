689a714ab74fc254d87f3b20e156d3d011e15b72
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-28 20:18:41 -0800

    Function | Added More Functions

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -27,11 +27,14 @@
 	 * @return string
 	 */
 	function output_messagev1( $message = "" ) {
+		
 		$output = "";
-		$output .= "<div class=\"alert-message success\">";
-     	$output .= "<a class=\"close\" id=\"closex\">×</a>";
+		$output .= "<div id=\"alertsuccess\" class=\"alert alert-success\">";
+     	$output .= "<a class=\"nav-text pull-right close-mark\">";
+     	$output .= "<button type=\"button\" class=\"close\" id=\"closex\" aria-hidden=\"true\">&times;</button></a>";
      	$output .= "<h4 class=\"list-group-item-heading\">{$message}</h4>";
      	$output .= "</div>";
+     	
      	return $output;
 	}
 
@@ -42,11 +45,32 @@
 	 * @return string
 	 */
 	function output_messagev2( $message = "" ) {
+		
 		$output = "";
-		$output .= "<p class=\"alert-message error\">";
-     	$output .= "<a class=\"close\" id=\"closex\" >×</a>";
+		$output .= "<p id=\"alertdanger1\" class=\"alert alert-danger\">";
+     	$output .= "<a class=\"nav-text pull-right close-mark\">";
+     	$output .= "<button type=\"button\" class=\"close\" id=\"closex\" aria-hidden=\"true\">&times;</button></a>";
      	$output .= "{$message}";
      	$output .= "</p>";
+     	
+     	return $output;
+
+	}
+
+	/**
+	 * Output Error Message.
+	 * 
+	 * @param string $message, string $modalname
+	 * @return string
+	 */
+	function output_messagev3( $message = "", $modalname = "" ) {
+		
+		global $globalsearchtext;
+						    				  
+		$output = "";
+		$output .= "<div class=\"alert alert-info\">";
+     	$output .= "<div class=\"row\"<h3 class=\"pull-left\"id=\"infosearch\">{$message}<a href={$modalname} data-toggle=\"modal\" class=\"btn-link pull-right\"> Search Again?</a></h3>";
+     	$output .= "</div></div>";
      	return $output;
 	}
 
@@ -57,13 +81,16 @@
 	 * @throws error message if file not found in the directory
 	 */
 	function __autoload( $class_name ) {
+		
 		$class_name = strtolower( $class_name );
 		$class_path = PPATH1.DS."{$class_name}";
+		
 		if ( file_exists( $filename ) ) {
 			require_once( $class_path );
 		} else {
 			die( "{$class_path}.php not found" );
 		}
+
 	}
 
 	/**
@@ -72,7 +99,9 @@
 	 * @param string $template
 	 */
 	function include_temp( $template = "" ) {
+
 		include( PPATH3.DS."{$template}" );
+
 	}
 
 ?>
