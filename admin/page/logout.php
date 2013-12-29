eaf324edc625053598a8b2a0d1a3cf985cb24502
Arvin Kent Lazaga <arvinkent17@gmail.com>
2013-12-29 03:54:58 -0800

    Logout Function | Just Added Some Condition

--- admin/page/logout.php
+++ admin/page/logout.php
@@ -5,6 +5,8 @@
 	 */
 	require_once("../includes/functions/initialize.php");
 	
+	if( !$admin_session->is_logged_in() ) { redirect_to("login.php"); }
+
 	$admin_session->logout();
 
 	redirect_to("login.php?logout=1");
